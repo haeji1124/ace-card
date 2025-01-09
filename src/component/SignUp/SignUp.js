bfc58439cc01e1da664e0ce825b08294e6590413
haeji1124 <46364329+haeji1124@users.noreply.github.com>
2025-01-09 19:10:08 +0900

    Update SignUp.js

--- src/component/SignUp/SignUp.js
+++ src/component/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import AuthContext from "../../store/auth-context";
 import { useNavigate } from "react-router-dom";
 
